a3a8fc2427d4fa64707da4e202a2721bd2e0355d
ronniedada <ronniedada@gmail.com>
2012-10-12 11:45:36 -0700

    use get for hot load phase

--- pytests/performance/perf.py
+++ pytests/performance/perf.py
@@ -657,6 +657,11 @@ class PerfBase(unittest.TestCase):
         # phase: 'load' or 'reload'
         phase = "load"
         if self.parami("hot_load_phase", 0) == 1:
+            # all gets
+            cfg['ratio-sets'] = 0
+            cfg['exit-after-creates'] = 0
+            cfg['exit-after-gets'] = 1
+            cfg['max-gets'] = start_at + num_items
             phase = "reload"
 
         if is_eperf:
